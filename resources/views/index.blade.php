00de0c2da2569d281c8fbc03f3c48984f5a5f3d2
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-27 16:19:43 +0200

    test for strucure of table

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,32 @@
     </head>
     <body>
         <h1 class="text-center">Treni in partenza</h1>
+
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Compagnia</th>
+                <th scope="col">Stazione di Partenza</th>
+                <th scope="col">Stazione di arrivo</th>
+                <th scope="col">Orario di partenza</th>
+                <th scope="col">Orario di arrivo</th>
+                <th scope="col">Codice treno</th>
+                <th scope="col">Carrozze</th>
+                <th scope="col">In Oraio</th>
+                <th scope="col">Cancellato</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($trains as $train)
+                <tr>
+                  <th scope="row">{{ $train->company }}</th>
+                  <td>{{ $train->departure_railway_station }}</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                @endforeach
+            </tbody>
+          </table>
         
     </body>
 </html>
