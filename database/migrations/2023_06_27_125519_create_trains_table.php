c83414fb9e8ef5e4a247ea4ac7eb9422845ca470
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-27 14:58:35 +0200

    add company in migration

--- database/migrations/2023_06_27_125519_create_trains_table.php
+++ database/migrations/2023_06_27_125519_create_trains_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
             $table->timestamps();
         });
     }
