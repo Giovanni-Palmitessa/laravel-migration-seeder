9d1b6a2f53ff5472f24a0f62f70fe492d4229c89
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-06-27 15:21:31 +0200

    add faker in seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
